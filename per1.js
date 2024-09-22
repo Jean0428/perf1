a76df17168f8cc366ac28074a993eff5fee92f6e
Jean Matbagan <matbaganjean@gmail.com>
2024-09-22 20:15:01 +0800

    Add files via upload

---
+++ per1.js
@@ -0,0 +1,12 @@
+let j = "1. Differentiation of let, const, and var in   JavaScript Instructions";
+let a = "The let, const, and var are used to declare a variable in JavaScript but there are differences between the three. ";
+let b = ["In let variables declared have Block Scope, variables must be declared before used, variables cannot be redeclared in the same scope, and it can be reassign. "];
+let c = ["In const ariables declared have also Block Scope, variables cannot be redeclared in the same scope, variables must be declared before used, and it cannot be reassign. "];
+let d = ["While in var,	variables declared always have Global Scope, variables can be redeclared, and it can be reassign. "]
+console.log(j);
+console.log(a + b + c + d);
+
+let e = "2.Concept of Falsy Values in JavaScript Instructions";
+let f = "In JavaScript, falsy value are those value is equal to false when converted to Boolean context, such as if statement and while (conditional statement). The concept of falsy value is when the value of the condition statement is expected to be false or it has no value, the JavaScript will automatically convert the value to false. Here are the 3 examples of falsy values: “”, 0, and null. “” (Empty string) considered as falsy because it shows the absent of textual context or value. 0 (number zero) considered as falsy because it shows the absent of numerical value (0 when used in conditional statemen is always consider as false). null considered as falsy because null represent the absence of any object value.  ";
+console.log(e);
+console.log(f);
